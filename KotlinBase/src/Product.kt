ba1d4c3bebde468c31bf0854e4ef0ba77f2e32b3
thanhtuanit <thanhtuanit.vn@gmail.com>
2025-09-30 17:02:54 +0700

    add the comment for product service

--- KotlinBase/src/Product.kt
+++ KotlinBase/src/Product.kt
@@ -1,21 +1,45 @@
+/**
+ * Class contain the product info.
+ */
 data class Product(val id: Int = 0, val name: String, val price: Double)
+
+/**
+ * The class handle all services related to the product.
+ */
 class  ProductService(){
+    /**
+     * The product list to handle
+     */
     private val  products = mutableListOf<Product>()
 
+    /**
+     * The func is used to add new product.
+     * @param item New product information.
+     */
     fun addProduct(item: Product){
         this.products.add(item)
     }
 
+    /**
+     * The func is used to print all product information to console screen.
+     */
     fun printAllProduct(){
         for (item in products) {
             println("${item.id} - ${item.name} - ${item.price}")
         }
     }
 
+    /**
+     * The func is used to remove a product by id from product list.
+     * @param id The product id will be removed from the list.
+     */
     fun removeProduct(id: Int){
         products.removeIf { it.id == id }
     }
 
+    /**
+     * Filter all products with price greater than or equal to the entered price.
+     */
     fun  filterProductByPrice(price: Double){
         val filtered = products.filter { it.price >= price }
     }
