82809b9c3af1b43f98662fbee0b80783f385a99f
thanhtuanit <thanhtuanit.vn@gmail.com>
2025-10-13 10:05:50 +0700

    Di chuyển giữa các màn hình bằng tính năng Compose

---
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/ui/form/Cupcake/Cupcake.kt
@@ -0,0 +1,8 @@
+package com.example.happybirthday.ui.form.Cupcake
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CupcakeScreen(){
+    
+}
---
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/ui/form/Cupcake/components/CommonUi.kt
@@ -0,0 +1,19 @@
+package com.example.happybirthday.ui.form.Cupcake.components
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.happybirthday.R
+
+/**
+ * Composable that displays formatted [price] that will be formatted and displayed on screen
+ */
+@Composable
+fun FormattedPriceLabel(subtotal: String, modifier: Modifier = Modifier) {
+    Text(
+        text = stringResource(R.string.subtotal_price, subtotal),
+        modifier = modifier,
+        style = MaterialTheme.typography.headlineSmall
+    )
+}
